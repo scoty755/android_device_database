79245f93d86cf629f032c2d3161d39edfac9a781
scoty755 <scoty755@gmail.com>
2014-04-20 20:22:51 +0900

    fix address for L01F

--- db_init.sql
+++ db_init.sql
@@ -1065,7 +1065,7 @@ insert into supported_devices(device_id, device, build_id, check_property_name, 
 
 
 insert into supported_devices(device_id, device, build_id, check_property_name, check_property_value) values(191, 'L01F', 'JDQ39B', 'ro.lge.swversion', 'L01F11k');
-  insert into device_address(device_id, name, value) values(191, 'prepare_kernel_cred', 30003033650);
+  insert into device_address(device_id, name, value) values(191, 'prepare_kernel_cred', 3222026152);
   insert into device_address(device_id, name, value) values(191, 'commit_creds', 3222024860);
   insert into device_address(device_id, name, value) values(191, 'remap_pfn_range', 3222555680);
   insert into device_address(device_id, name, value) values(191, 'vmalloc_exec', 3222610396);
